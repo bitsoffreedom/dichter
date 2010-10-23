4033e8277da365816e6c1f2d33fc7a97b1a9d23a
janklopper <janklopper@cavia.(none)>
2010-10-24 00:56:20 +0200

    use a sane default standard campaign

--- denhaag/views.py
+++ denhaag/views.py
@@ -4,19 +4,23 @@ from django.conf import settings
 from dichter.denhaag.models import *
 from django.http import HttpResponse
 
-def index(request, campaign_slug='stop-de-zwarte-lijst'):
-	try:
-        	c = Campaign.objects.get(title_slug=campaign_slug)
-	except Campaign.DoesNotExist:
-        	raise Http404
+def index(request, campaign_slug=None):
+  try:
+    if not campaign_slug:
+      c = Campaign.objects.all().order_by('-start_date')[:1]
+    else:
+      c = Campaign.objects.get(title_slug=campaign_slug)
+  except Campaign.DoesNotExist:
+    
+    raise Http404
 
-	campaign_list = Campaign.objects.all().order_by('-start_date')[:5]
-	politici_list = Politician.objects.all()
-	for politicus in politici_list:
-	  politicus.contact_methods = PoliticianContactInfo.objects.filter(politician=politicus)
+  campaign_list = Campaign.objects.all().order_by('-start_date')[:5]
+  politici_list = Politician.objects.all()
+  for politicus in politici_list:
+    politicus.contact_methods = PoliticianContactInfo.objects.filter(politician=politicus)
 	
-	t = loader.get_template('index.html')
-	return render_to_response('index.html', {'campaign_list': campaign_list, 'politici_list': politici_list, 'STATIC_PREFIX': settings.MEDIA_URL, 'campaign': c})
+  t = loader.get_template('index.html')
+  return render_to_response('index.html', {'campaign_list': campaign_list, 'politici_list': politici_list, 'STATIC_PREFIX': settings.MEDIA_URL, 'campaign': c})
 
 def campaign_detail(request, campaign_id):
     try:
