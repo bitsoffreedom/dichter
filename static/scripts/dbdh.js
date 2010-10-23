bf076c6d779dd5d8d7a52d9fa4d8cde6d9d7a536
Arnoud Vermeer <a.vermeer@freshway.biz>
2010-10-23 18:49:28 +0200

    Merge van tychobismeijer/dbdh met bitsoffreedom/dichter

---
+++ static/scripts/dbdh.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	$('#hero .extraInfo').hide();
+	$('#meer a').click(function(){
+		$('#hero .extraInfo').slideToggle();
+		$('#meer').toggleClass('open');
+		$(this).text($(this).text() == "Meer lezen" ? "Verbergen" : "Meer lezen");
+
+	});
+
+	$('#targets .extraInfo').hide();
+	$('#targets .infoToggle a').click(function(e){
+		e.preventDefault();
+		$(this).closest('.infoToggle').prev().slideToggle();
+		$(this).closest('.infoToggle').toggleClass('hide');
+		$(this).text($(this).text() == "Meer informatie" ? "Informatie verbergen" : "Meer informatie");
+	});
+	
+});
---
+++ static/scripts/jquery.scrollabletab.init.js
@@ -0,0 +1,4 @@
+$(function () {
+		 $('#campagnes-list').tabs().scrollabletab()
+})
+
