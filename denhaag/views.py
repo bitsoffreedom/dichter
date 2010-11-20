82e688785f25f0664340ba81ec92fd98c039fcde
janklopper <janklopper@cavia.(none)>
2010-11-20 19:51:38 +0100

    import the http404 error class

--- denhaag/views.py
+++ denhaag/views.py
@@ -2,7 +2,7 @@ from django.template import Context, loader
 from django.shortcuts import render_to_response
 from django.conf import settings
 from dichter.denhaag.models import *
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 # non django imports
 import datetime
