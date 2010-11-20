6f1a0ad9d73fa298b0c8f9689ef7cb17cce6b0aa
Ilja (Ikarus) Kamps <ikarus@ikarus.ath.cx>
2010-11-20 22:16:27 +0000

    Revert "Fixed down scroll, selected scroll, etc for targets"
    This reverts commit bf605257da4d21002ffba0cee88849cfc34e79db.

--- static/scripts/jquery.scrollabletargets.js
+++ static/scripts/jquery.scrollabletargets.js
@@ -25,14 +25,13 @@ $(function () {
 		var firstPartialTop = $.grep($("#targets > li"), function (e) {
 			return ($(e).position().top < $("#targets").position().top) 
 		}).reverse()[0]
-		$(firstPartialTop).trigger("scrollTo"); 
+		$(firstPartialTop).trigger("scrollTo");
 	}
 	function scrollDown (e) {
 		var firstPartialBottom = $.grep($("#targets > li"), function (e) {
 			return (($(e).position().top + $(e).height()) > ($("#targets").position().top + $("#targets").height()) ) 
 		})[0]
-		$(firstPartialBottom).trigger("scrollTo"); 
-		
+		$(firstPartialBottom).trigger("scrollTo");
 	}
 
 	$("#targetScrollUp").click(scrollUp);
@@ -40,16 +39,10 @@ $(function () {
 	$("#targets").scroll(navEnabler);
 
 	var targets = $("#targets > li");
-	b
-	for (var i = 0; i < targets.length; i++) { 
+	for (var i = 0; i < targets.length; i++) {
 		$(targets[i]).bind("scrollTo", function (e) { 
-			if ($(e.currentTarget).position().top < $("#targets").position().top) {
-				$(e.currentTarget).parent("ul").scrollTo(e.currentTarget, 500, {offset: {top: -12, left: 0} })
-			} else if (($(e.currentTarget).position().top + $(e.currentTarget).height()) > ($("#targets").position().top + $("#targets").height())) {
-				$(e.currentTarget).parent("ul").scrollTo(e.currentTarget, 500, {offset: {top: 20 - ( $("#targets").height() - $(e.currentTarget).height() ), left: 0} })
-			}
+			$(e.currentTarget).parent("ul").scrollTo(e.currentTarget, { duration: 500 })
 		});
-
 		$(targets[i]).click(function (e) {
 			$(e.currentTarget).trigger("scrollTo");
 		});
